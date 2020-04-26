80f2b9d63eb0a777533f0593ed7aa256cb10c4a8
zhangjufeng <zjffun@gmail.com>
2020-04-26 10:11:03 -0400

    get some fail and timeout tests, update update-to-run.js

--- test/web-platform-tests/tools/update-to-run.js
+++ test/web-platform-tests/tools/update-to-run.js
@@ -1,22 +1,23 @@
+// Need?
+// using yaml AST?
+
 "use strict";
+const readline = require("readline");
 const path = require("path");
 const fs = require("fs");
-const childProcess = require("child_process");
+const os = require("os");
 const jsYAML = require("js-yaml");
 const Mocha = require("mocha");
 
 const mocha = new Mocha({});
 
-const command = `git submodule status ${resolveWpt("tests")} | awk '{ printf $1 }'`;
-const wptSHA1 = String(childProcess.execSync(command));
-
 const toRunFilename = resolveWpt("to-run.yaml");
 const toRunString = fs.readFileSync(toRunFilename, { encoding: "utf-8" });
-const toRunDocs = jsYAML.safeLoad(toRunString, { filename: toRunFilename });
+const toRunDocs = jsYAML.safeLoadAll(toRunString, { filename: toRunFilename });
 const toRunObj = {};
 
 toRunDocs.forEach(doc => {
-  toRunObj[doc.DIR] = doc;
+  toRunObj[doc.DIR] = Object.entries(doc);
 });
 
 mocha.addFile(resolveWpt("run-wpts.js"));
@@ -26,22 +27,66 @@ mocha
   .run()
   .on("fail", (test, err) => {
     if (err.message.startsWith("Error: test harness should not timeout:")) {
-      reason = ["timeout", `Unknown ${wptSHA1}`];
+      reason = "[timeout, Unknown-test]";
     } else {
-      reason = ["fail", `Unknown ${wptSHA1}`];
+      reason = "[fail, Unknown-test]";
     }
-    toRunObj[test.parent.title][test.title] = reason;
+    toRunObj[test.parent.title].push([test.title, reason]);
   })
   .on("end", () => {
-    fs.writeFileSync(
-      resolveWpt("to-run.yaml"),
-      jsYAML.safeDump(toRunDocs, {
-        sortKeys(a, b) {
-          return b === "DIR" || a > b ? 1 : -1;
-        }
-      })
-    );
+    updateToRun();
   });
+
+async function updateToRun() {
+  const fileStream = fs.createReadStream(resolveWpt("to-run.yaml"));
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity
+  });
+
+  const toRunFlatArr = [];
+  const newToRunStrings = [];
+
+  for (const toRunArr of Object.values(toRunObj)) {
+    toRunArr
+      .sort((a, b) => {
+        return b[0] === "DIR" || a[0] > b[0] ? 1 : -1;
+      })
+      .forEach(test => {
+        toRunFlatArr.push(test);
+      });
+  }
+
+  let test = toRunFlatArr.shift();
+  for await (const line of rl) {
+    if (line === "---") {
+      while (test && test[0] !== "DIR") {
+        newToRunStrings.splice(-1, 0, `${test[0]}: ${test[1]}`);
+        test = toRunFlatArr.shift();
+      }
+    } else if (line === "" || line.startsWith(" ")) {
+      // do nothing
+    } else if (test && !line.startsWith(test[0])) {
+      while (test && !line.startsWith(test[0])) {
+        newToRunStrings.push(`${test[0]}: ${test[1]}`);
+        test = toRunFlatArr.shift();
+      }
+      test = toRunFlatArr.shift();
+    } else {
+      test = toRunFlatArr.shift();
+    }
+    newToRunStrings.push(line);
+  }
+
+  while (test) {
+    newToRunStrings.push(`${test[0]}: ${test[1]}`);
+    test = toRunFlatArr.shift();
+  }
+
+  newToRunStrings.push("");
+
+  fs.writeFileSync(resolveWpt("to-run.yaml"), newToRunStrings.join(os.EOL));
+}
 
 function resolveWpt(...paths) {
   return path.resolve(__dirname, "../", ...paths);
